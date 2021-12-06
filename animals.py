191e536ad0313f4232d7475e8af7b08bbf0f64bc
latsame <93645294+latsame@users.noreply.github.com>
2021-12-05 22:33:51 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,14 @@
+
+animal_1 = int(input('Enter number animals'))
+
+if animal_1 == 1:
+    print('Animals  is', animal_1, 'Cat') 
+
+elif animal_1 == 2:
+    print('Animals  is', animal_1,'Dog') 
+
+elif animal_1 == 3:
+    print('Animals  is', animal_1,' Tiger')
+
+else :
+    print( 'I do not know ')
